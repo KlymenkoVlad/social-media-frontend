8c34b8568f8942b32357efe3849ab6a238672d66
Vlad Klymenko <klymenvlad@gmail.com>
2024-06-22 13:30:53 +0200

    fix hydration bug

--- app/_actions.ts
+++ app/_actions.ts
@@ -92,7 +92,7 @@ export const getMe = async () => {
 export const getAllPosts = async (
   cursor?: number,
   sortBy = "new",
-  take = 3,
+  take = 4,
 ) => {
   const token = cookies().get("token")?.value;
 
@@ -264,7 +264,7 @@ export const updatePassword = async (data: PasswordFormInputs) => {
 export const findUserByUsername = async (
   username: string,
   cursor?: number,
-  take = 3,
+  take = 12,
 ) => {
   const token = cookies().get("token")?.value;
 
